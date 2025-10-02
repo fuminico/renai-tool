639779d9f48d6db7d72f4a65096897a6f12b9e68
fuminico <fuminico@gmail.com>
2025-10-02 14:40:27 +0900

    fix: vite.config.jsの構文エラーを修正

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,17 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig({
-  base: '/renai-tool/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/renai-tool/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: 3005,
-  },
+    server: {
+      port: 3005,
+    },
+  }
 })
