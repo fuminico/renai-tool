ffec8250f4a01439b732c33fb4518fc3c2e60ac6
fuminico <fuminico@gmail.com>
2025-10-02 14:51:33 +0900

    feat: デプロイ方法をvite-plugin-gh-pagesに変更

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? '/renai-tool/' : '/',
-    plugins: [react()],
+    plugins: [react(), ghPages()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
